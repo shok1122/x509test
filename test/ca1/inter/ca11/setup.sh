3a9683a81add3866342085ff7c4eca25a8c74a81
shok1122 <shok1122@zohomail.com>
2021-03-20 12:03:39 +0900

    Added certificate authorities for test

---
+++ test/ca1/inter/ca11/setup.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+ca_dir="materials"
+
+rm -rf $ca_dir/*
+mkdir $ca_dir/certs
+mkdir $ca_dir/crl
+mkdir $ca_dir/newcerts
+mkdir $ca_dir/private
+mkdir $ca_dir/cache
+touch $ca_dir/index.txt
+echo "01" > serial
+echo "00" > crlnumber
+
+openssl genrsa \
+    -passout pass:pass \
+    -aes256 \
+    -out $ca_dir/private/cakey.pem \
+    2048
+
+openssl req -new \
+    -key $ca_dir/private/cakey.pem \
+    -out cache/careq.pem \
+    -subj "/C=JP/ST=Aichi/L=Nagoya/O=CA11/CN=ca11.com" \
+    -passin pass:pass
+
+dir=$PWD
+(
+    cd ../../
+    openssl ca \
+        -config openssl.cnf \
+        -batch \
+        -extensions v3_ca \
+        -out $dir/$ca_dir/cacert.pem \
+        -in $dir/cache/careq.pem \
+        -cert $ca_dir/cacert.pem \
+        -keyfile $ca_dir/private/cakey.pem \
+        -passin pass:pass \
+)
+
---
+++ test/ca1/inter/ca11/verify-cacert.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+openssl verify \
+    -CAfile ../../materials/cacert.pem \
+    materials/cacert.pem
+
